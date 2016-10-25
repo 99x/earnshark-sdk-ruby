def862550f40ceea100ffeb115eaed62097a079e
Chamath Palihawadana <chamathpali123@gmail.com>
2016-10-25 23:23:18 +0530

    Added test implementation with the isTest/Debug mode and dummy responses setup to test endpoint functions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,67 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "earnshark_sdk"
 
 product_id = 0
+
 key = "test"
+
+body = '{
+            "account":{
+                    "name":"Account Name",
+                    "email":"Account@example.com",
+                    "accountID":"123456",
+                    "start_date":"01/01/2016"
+                },
+                "license_id":0,
+                "enableNotifications" : false,
+                "sendInvoiceNow" : true
+         }'
+
+account_id = 0
+
 
 earnshark = EarnShark::API.new(product_id, key)
 
-res= earnshark.get_obj
+earnshark.isTest(true)
 
-puts res
+addNewRes= earnshark.add_new_subscription(body)
+
+puts "\n"
+puts "Add new account Response:\n #{addNewRes}"
+
+
+accountInfo= earnshark.get_account_information(account_id)
+puts "\n\n"
+puts "Get account information:\n #{accountInfo}"
+
+
+subscription_id = 0
+new_license_id =0
+
+renew_subs= earnshark.renew_subscription( subscription_id, new_license_id )
+puts "\n\n"
+puts "Renew Subscripiton with a new license:\n #{renew_subs}"
+
+license_id = 0
+license_token = ''
+
+get_license= earnshark.get_license_information( license_id, license_token )
+puts "\n\n"
+puts "Get License Information:\n #{get_license}"
+
+all_licenses= earnshark.get_all_licenses_of_product()
+puts "\n\n"
+puts "All Licenses information:\n #{all_licenses}"
+
+
+account_transaction= earnshark.get_account_payments(account_id)
+puts "\n\n"
+puts "Payment Transactions of account:\n #{account_transaction}"
+
+
+redirect = 'http://app.earnshark.com';
+payment_url= earnshark.get_payment_url(account_id, redirect)
+puts "\n\n"
+puts "Payment URL for an account:\n #{payment_url}"
+
+
+
