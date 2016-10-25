13486d0c18f6c08a5af47b5f11296436ba9900dd
Chamath Palihawadana <chamathpali123@gmail.com>
2016-10-25 11:22:18 +0530

    Set the User credentials (Token, Product ID ) and intialise object, added rspec test data

--- lib/earnshark_sdk.rb
+++ lib/earnshark_sdk.rb
@@ -1,5 +1,15 @@
 require "earnshark_sdk/version"
 
-module EarnsharkSdk
-  # Your code goes here...
+module EarnShark
+  class API
+
+    #initialize the object with the earnshark product id and token provided
+  	def initialize(product_id,key)
+  		@product_id, @key = product_id , key
+  	end
+
+  	def get_obj
+  		"product_id = #@product_id , token = #@key"
+  	end
+  end
 end
