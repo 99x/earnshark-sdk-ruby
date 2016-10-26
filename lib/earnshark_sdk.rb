dc0c283b8a05fc500a9a62a481b92fa1b273fcfb
Chamath Palihawadana <chamathpali123@gmail.com>
2016-10-26 11:54:03 +0530

    Major Updates. added HTTP module and integrated Earnshark API endpoints . added test functions

--- lib/earnshark_sdk.rb
+++ lib/earnshark_sdk.rb
@@ -1,4 +1,5 @@
 require "earnshark_sdk/version"
+require "http"
 
 module EarnShark
   class API
@@ -9,7 +10,7 @@ module EarnShark
   		@isTest = false
   		#URLS
   		@baseURL = 'https://app.earnshark.com/prod/product/';
-		@appDir = 'http://earnsharkbeta.com.s3-website-eu-west-1.amazonaws.com/';
+		  @appDir = 'http://earnsharkbeta.com.s3-website-eu-west-1.amazonaws.com/';
   	end
 
   	def isTest(yes)
@@ -35,8 +36,9 @@ module EarnShark
   		if(@isTest)
   			'[ { "Date":"01/01/2016", "Cost":{ "value":"100", "currency":"USD" }, "Name":"5 Users Plan", "Product_ID":1, "Customer_ID":1, "Customer_Name":"Customer Name", "Account_ID":"123456", "Metadata":"{\"users\":\"10 users\"}", "Subscription_ID":1, "Tags":[ "10users" ], "License_ID":1, "Invalid":false, "Expired":false, "Billing_Cycle":{ "value":1, "type":"Monthly" }, "Description":"10 Users per month / 100", "Tenant_ID":"eu-west-1:00000000-0000-0000-0000-000000000000" } ]'
   		else
-  			#http request comes here
-
+        url = "#@baseURL#@product_id/subscriptioninfo/#{account_id}?key=#@key"
+        response = HTTP.get(url).body
+        "#{response}"
   		end  		
   	end
 
@@ -46,8 +48,9 @@ module EarnShark
   		if(@isTest)
   			'{ "Name":"License Name", "Cost":{ "value":"100", "currency":"USD" }, "Tags":[ "10 User Package" ], "License_ID":1, "Product_ID":1, "Description":"10 Users per month / 100", "Billing_Cycle":{ "value":1, "type":"Monthly" }, "Metadata":"{\"users\":\"10\"}" }'
   		else
-  			#http request comes here
-
+  			url = "#@baseURL#@product_id/license/#{license_id}/getlicensefromapi?key=#{license_token}"
+        response = HTTP.get(url).body
+        "#{response}"
   		end  		
   	end
 
@@ -69,8 +72,9 @@ module EarnShark
   		if(@isTest)
   			'[{"Name":"License Name","Cost":{"value":"10","currency":"USD"},"Tags":["free","test","trial"],"License_ID":1,"Product_ID":1,"Description":"Test","Billing_Cycle":{"value":1,"type":"Monthly"},"Metadata":"{}"}]'
   		else
-  			#http request comes here
-
+        url = "#@baseURL#@product_id/license/all?key=#@key"
+        response = HTTP.get(url).body
+        "#{response}"
   		end  		
   	end
 
@@ -80,8 +84,9 @@ module EarnShark
   		if(@isTest)
   			'[ { "Amount":"100", "Created_At":"1474872974062", "Currency":"USD", "Product_ID":1, "PayPal_Transaction":"PAY-11111111111", "Updated_At":"1474872974062", "Account_ID":"local", "PayPal_Payer_ID":"111111", "Subscription_ID":1, "Payment_Processed":true, "Payout_ID":"12345", "Payment_Sent":true, "PayPal_Payment_ID":"PAY-11111111111" } ]'
   		else
-  			#http request comes here
-
+  		  url = "#@baseURL#@product_id/account/#{account_id}/transactions?key=#@key"
+        response = HTTP.get(url).body
+        "#{response}"
   		end  		
   	end
 
